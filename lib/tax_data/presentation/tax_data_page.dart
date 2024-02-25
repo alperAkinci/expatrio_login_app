9036bbaee056f738390254c596d91d26e70c4905
Alper Akinci <alperakinci1@gmail.com>
2024-02-25 14:45:32 +0100

    Update UI of tax data page

--- lib/tax_data/presentation/tax_data_page.dart
+++ lib/tax_data/presentation/tax_data_page.dart
@@ -1,10 +1,5 @@
-import 'package:dropdown_search/dropdown_search.dart';
-import 'package:expatrio_login_app/shared/countries_constants.dart';
-import 'package:expatrio_login_app/shared/item_dropdown.dart';
-import 'package:expatrio_login_app/tax_data/model/tax_data.dart';
 import 'package:expatrio_login_app/tax_data/presentation/tax_data_modal_sheet.dart';
 import 'package:expatrio_login_app/tax_data/provider/tax_data_notifier.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
@@ -20,15 +15,16 @@ class TaxDataPage extends StatefulWidget {
 class _TaxDataPageState extends State<TaxDataPage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: use font with bold style
-    final titleLargeStyle = Theme.of(context).textTheme.bodyLarge;
+    var textTheme = Theme.of(context).textTheme;
+    final titleStyle = textTheme.titleMedium!.copyWith(
+      fontWeight: FontWeight.bold,
+    );
     return Scaffold(
         appBar: AppBar(),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 80.0),
+        body: Container(
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 SvgPicture.asset(
                   "assets/CryingGirl.svg",
@@ -38,17 +34,20 @@ class _TaxDataPageState extends State<TaxDataPage> {
                 const SizedBox(height: 10),
                 Text(
                   "Uh-Oh!",
-                  style: titleLargeStyle,
+                  style: titleStyle,
                 ),
                 const SizedBox(height: 8),
-                Text(
-                  "We need your tax data in order to access your account",
-                  style: titleLargeStyle,
-                  textAlign: TextAlign.center,
+                SizedBox(
+                  width: 200,
+                  child: Text(
+                    "We need your tax data in order to access your account",
+                    style: textTheme.bodyMedium,
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-                const SizedBox(height: 20),
-                Container(
-                  width: double.infinity,
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: 250,
                   child: FilledButton(
                     onPressed: () {
                       Provider.of<TaxDataNotifier>(context, listen: false)
